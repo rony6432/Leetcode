e78d08e948cc1f7bc760c442a6e207a9481e2805
Rony Sarkar <119131859+rony6432@users.noreply.github.com>
2024-07-09 11:12:22 +0530

    Time: 3 ms (43.26%), Space: 7.5 MB (78.67%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,40 +1,47 @@
 class Solution {
 public:
-#define mod 1000000007
-    long long hash(string need,int m,int radix){
-        long long ans=0,factor=1;
-        for(int i=m-1;i>=0;i--){
-            ans=ans+((need[i]-'a')*factor)%mod;
-            factor=(factor*radix)%mod;
+vector<int>lps(string need){
+    int n=need.size();
+    vector<int>ans(n,0);
+    int len=0,i=1;
+    while(i<n){
+        if(need[i]==need[len]){
+            len=len+1;
+            ans[i]=len;
+            i++; 
         }
-        return ans%mod;
+        else{
+            if(len!=0)
+            len=ans[len-1];
+            else{
+                ans[i]=len;
+                i++;
+            }
+        }
     }
-    int rab_caf(string hay,string need){
-        int n=hay.size();
-        int m=need.size();
-        int radix=27;
-        long long power=1;
-        for(int i=0;i<m;i++) power=(power*radix)%mod;
-        long long need_hash=hash(need,m,radix);
-        long long hay_hash;
-        for(int i=0;i<=n-m;i++){
-            if(i==0){
-                hay_hash=hash(hay,m,radix);
+    return ans;
+}
+    int strStr(string haystack, string needle) {
+        int n=haystack.size();
+        int m=needle.size();
+        vector<int>lps_c=lps(needle);
+        int j=0;
+        int i=0;
+        while(i<n){
+            if(haystack[i]==needle[j]){
+                j++;
+                i++;
             }
             else{
-                hay_hash=((hay_hash*radix)%mod-((hay[i-1]-'a')*power)%mod+(hay[i+m-1]-'a')+mod)%mod;
-            }
-            if(hay_hash==need_hash){
-                for(int j=0;j<m;j++){
-                    if(need[j]!=hay[i+j]) break;
-                    if(j==m-1) return i;
+                if(j!=0){
+                    j=lps_c[j-1];
+                    }
+                else {
+                    i++;
                 }
             }
+            if(j==m) return i-j;
         }
         return -1;
-    }
-    int strStr(string haystack, string needle) {
-        if(haystack.size()<needle.size()) return -1;
-        return rab_caf(haystack,needle);
     }
 };
